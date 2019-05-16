0f53b207c46090c006386b8db4f1dd60ed182ae6
Tedy Ir <siiberad@gmail.com>
2019-05-16 18:08:08 +0700

    master

---
+++ app/src/main/java/com/siiberad/eventbustest/Fragment1.java
@@ -0,0 +1,35 @@
+package com.siiberad.eventbustest;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.greenrobot.eventbus.EventBus;
+
+public class Fragment1 extends AppCompatActivity {
+
+    EditText childActivityEditText;
+    Button childActivityButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_2);
+        // init the views
+        childActivityEditText = (EditText) findViewById(R.id.childactivity_edittext);
+        childActivityButton = (Button) findViewById(R.id.childactivity_button);
+        // set the onclick
+        childActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MessageEvent messageEvent = new MessageEvent();
+                messageEvent.setMessage(childActivityEditText.getText().toString());
+
+                EventBus.getDefault().post(messageEvent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/siiberad/eventbustest/MessageEvent.java
@@ -0,0 +1,13 @@
+package com.siiberad.eventbustest;
+
+public class MessageEvent {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
